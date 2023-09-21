9f512cae1ba4220b18cef19ec0b4f5fcf3a04ac9
markopolo139 <marekseget@onet.pl>
2023-09-21 15:45:32 +0200

    feat(api): creat auth api

---
+++ src/api/AuthApi.ts
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import LoginModel from "../objects/LoginModel";
+
+export const authApi = createApi({
+    reducerPath: "authApi",
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:4200/'}),
+    tagTypes: ['Auth'],
+    endpoints: (builder) => ({
+        authenticate: builder.mutation<string, LoginModel>({
+            query: (model: LoginModel) => ({
+                url: "auth",
+                method: "POST",
+                body: model,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                responseHandler: (response) => response.text()
+            }),
+            invalidatesTags: ['Auth']
+        })
+    })
+})
+
+export const { useAuthenticateMutation } = authApi
