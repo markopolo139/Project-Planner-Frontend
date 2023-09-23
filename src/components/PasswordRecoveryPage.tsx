d7da60bc9a67663079f1e6da26f9c9d4b872289d
markopolo139 <marekseget@onet.pl>
2023-09-23 23:13:32 +0200

    feat(PasswordRecovery): add password verification

--- src/components/PasswordRecoveryPage.tsx
+++ src/components/PasswordRecoveryPage.tsx
@@ -1,16 +1,31 @@
-import {Button, CircularProgress, TextField} from "@mui/material";
+import {Alert, Button, CircularProgress, TextField} from "@mui/material";
 import {useState} from "react";
 import useQuery from "../configuration/QueryHook";
 import {useChangePasswordMutation, useSendEmailMutation} from "../api/RecoveryPasswordApi";
 import ErrorPopUp from "./ErrorPopUp";
 
 export default function PasswordRecoveryPage() {
-    const [sendEmail, { error: emailError, isLoading: isEmailSending }] = useSendEmailMutation()
-    const [changePassword, { error: changePasswordError, isLoading }] = useChangePasswordMutation()
+    const [
+        sendEmail,
+        { error: emailError, isLoading: isEmailSending }
+    ] = useSendEmailMutation()
+
+    const [
+        changePassword,
+        { error: changePasswordError, isLoading }
+    ] = useChangePasswordMutation()
 
     const [email, setEmail] = useState("")
     const [newPassword, setPassword] = useState("")
+    const [confirmNewPassword, setConfirmPassword] = useState("")
+    const [isFormValid, setValidForm] = useState(true)
     const token = useQuery().get("token")
+
+    function validateNewPassword() {
+        setValidForm(true)
+        if (newPassword !== confirmNewPassword)
+            throw new Error()
+    }
 
     if (token != null) {
         if (isLoading)
@@ -21,17 +36,29 @@ export default function PasswordRecoveryPage() {
                     <CircularProgress />
                 </div>
             )
-
         return (
             <div>
                 { changePasswordError && <ErrorPopUp error={changePasswordError}/> }
+                { isFormValid || <Alert severity="error">Password does not match</Alert> }
                 <form onSubmit={e => {
                     e.preventDefault()
-                    changePassword({ newPassword, token })
+                    try {
+                        validateNewPassword()
+                        changePassword({ newPassword, token })
+                    } catch (e: any) {
+                        setValidForm(false)
+                    }
                 }}>
                     <TextField
                         label="New Password" variant="outlined" type="password" onChange={e => {
                         setPassword(e.target.value.trim())
+                    }}
+                    />
+                    <br/>
+                    <br/>
+                    <TextField
+                        label="Confirm New Password" variant="outlined" type="password" onChange={e => {
+                        setConfirmPassword(e.target.value.trim())
                     }}
                     />
                     <br/>
