dacef04c46b500327036342f0cc9f52c7b85d7de
markopolo139 <marekseget@onet.pl>
2023-09-26 23:46:28 +0200

    feat(LandingPage): setting up email after logging in

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,12 +1,21 @@
-import {useSelector} from "react-redux";
-import {selectLoggedInUserData} from "../slices/LoggedInUserSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {selectLoggedInUserData, setLoggedInEmail} from "../slices/LoggedInUserSlice";
 import styles from "../css/LandingPage.module.sass";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import {useGetEmailQuery} from "../api/UserApi";
+import {useEffect} from "react";
 
 export default function LandingPage() {
-    const loggedInUser = useSelector(selectLoggedInUserData)
-    //TODO: add most recently project to show here or current project or with close deadline
+    const { data, isSuccess } = useGetEmailQuery();
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (isSuccess)
+            dispatch(setLoggedInEmail(data))
+    }, [data, dispatch, isSuccess])
+
+    //TODO: add most recently project to show here or current project or with close deadline,
     return (
         <div>
             <header className={styles.header}>Project Overview</header>
