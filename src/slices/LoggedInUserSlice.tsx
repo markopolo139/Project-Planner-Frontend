ec0af4536ef17e3ac1edc78881afaab2fc7f937e
markopolo139 <marekseget@onet.pl>
2023-09-26 23:29:51 +0200

    feat(LoggedInUserSlice): add email

--- src/slices/LoggedInUserSlice.tsx
+++ src/slices/LoggedInUserSlice.tsx
@@ -4,11 +4,13 @@ import {RootState} from "../configuration/Strore";
 interface LoggedInUserState {
     jwtToken: string
     username: string
+    email: string
 }
 
 const initialValue: LoggedInUserState = {
     jwtToken: "",
-    username: ""
+    username: "",
+    email: ""
 }
 
 export const loggedInUserSlice = createSlice({
@@ -21,15 +23,22 @@ export const loggedInUserSlice = createSlice({
         setLoggedInUsername: (state, action: PayloadAction<string>) => {
             state.username = action.payload
         },
+        setLoggedInEmail: (state, action: PayloadAction<string>) => {
+            state.email = action.payload
+        },
         logout: (state) => {
             state.username = ""
             state.jwtToken = ""
+            state.email = ""
         }
     }
 })
 
-export const { setJwtToken, setLoggedInUsername, logout } = loggedInUserSlice.actions
+export const {
+    setJwtToken, setLoggedInUsername, logout, setLoggedInEmail
+} = loggedInUserSlice.actions
 export const selectJwtToken = (state: RootState) => state.loggedInUser.jwtToken
-export const selectUsername = (state: RootState) => state.loggedInUser.jwtToken
+export const selectUsername = (state: RootState) => state.loggedInUser.username
+export const selectEmail = (state: RootState) => state.loggedInUser.email
 export const selectLoggedInUserData = (state: RootState) => state.loggedInUser
 export default loggedInUserSlice.reducer
