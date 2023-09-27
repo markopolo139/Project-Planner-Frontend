6e3c4880f164859e19ec67f93eed444aa879b17b
markopolo139 <marekseget@onet.pl>
2023-09-27 16:07:11 +0200

    feat(navbar): add css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,6 +10,7 @@ import {Link, useNavigate} from "react-router-dom";
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {logout, selectLoggedInUserData, selectUsername} from "../slices/LoggedInUserSlice";
+import styles from "../css/Navbar.module.sass"
 
 export default function Navbar() {
     const [
@@ -25,28 +26,59 @@ export default function Navbar() {
 
     function handleMenuClick(event: React.MouseEvent<HTMLElement>) {
         setAnchorEl(event.currentTarget)
+        event.currentTarget.classList.add(styles.menuClicked)
     }
 
     function handleMenuClose() {
+        anchorEl?.classList?.remove(styles.menuClicked)
         setAnchorEl(null)
     }
 
     return (
-        <div>
+        <div className={styles.mainDiv}>
             { isError && <ErrorPopUp error={error} /> }
             <h2>Project Overview</h2>
             <Link to={"projects"}>Projects</Link>
             <Link to={"project/plans"}>Project Plans</Link>
-            <div onClick={handleMenuClick}>
+            <div className={styles.menuDiv} onClick={handleMenuClick}>
                 <p>{username}</p>
                 <AccountCircleIcon />
             </div>
             <Menu
+                anchorEl={anchorEl}
                 open={open}
                 onClose={handleMenuClose}
                 onClick={handleMenuClose}
                 transformOrigin={{ horizontal: 'right', vertical: 'top' }}
                 anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                slotProps={{
+                    paper: {
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }
+                }}
             >
                 <MenuItem onClick= {(e) => {
                     getAppToken(addNotificationToken)
@@ -63,7 +95,7 @@ export default function Navbar() {
                     dispatch(logout())
                     navigate("/")
                 }}>
-                    <ListItemIcon><Logout /></ListItemIcon> LogOut
+                    <ListItemIcon><Logout /></ListItemIcon> Logout
                 </MenuItem>
             </Menu>
         </div>
