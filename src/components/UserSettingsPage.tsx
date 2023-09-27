95512a9ed4df43fde333b63f4db1de09b3a5a965
markopolo139 <marekseget@onet.pl>
2023-09-27 23:55:07 +0200

    feat(UserSettings): create user settings page without css

--- src/components/UserSettingsPage.tsx
+++ src/components/UserSettingsPage.tsx
@@ -1,8 +1,60 @@
+import {Link, Outlet} from "react-router-dom";
+import {useState} from "react";
+import {Button} from "@mui/material";
+import Popup from "reactjs-popup";
+import CloseIcon from '@mui/icons-material/Close';
+import {useDispatch} from "react-redux";
+import {logout} from "../slices/LoggedInUserSlice";
+import {useDeleteUserMutation} from "../api/UserApi";
+import MyError from "../objects/MyError";
+import ErrorPopUp from "./ErrorPopUp";
+
 export default function UserSettingsPage() {
-    // TODO: User setting will have children(change username/password/email) in router and use outlet also children will have retun button
+    const [deleteUser] = useDeleteUserMutation()
+    const [isDelete, setDelete] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [error, setError] = useState<any>(null);
+    const dispatch = useDispatch()
+
+    function handleDeleteConfirmClick(_: any) {
+        setDelete(false)
+        setOpen(false)
+        deleteUser().unwrap().then( fulfilled => dispatch(logout())).catch(rejected => setError(rejected))
+    }
+
     return (
         <div>
-
+            { error && <ErrorPopUp error={error} /> }
+            <div>
+                <Link onClick={(e) => {
+                    setDelete(false)
+                }} to={"username"}>Change Username</Link>
+                <Link onClick={(e) => {
+                    setDelete(false)
+                }} to={"password"}>Change Password</Link>
+                <Link onClick={(e) => {
+                    setDelete(false)
+                }} to={"email"}>Change Email</Link>
+                <p onClick={(e) => {
+                    setDelete(true)
+                }}>Delete Username</p>
+            </div>
+            <div>
+                { isDelete || <Outlet />}
+                { isDelete && <Button onClick={(e) => {
+                    setOpen(true)
+                }}>Delete User</Button>}
+                <Popup open={open}>
+                    <div>
+                        <h2>Are you sure</h2>
+                        <h3>All data will be lost</h3>
+                        <CloseIcon className="popup-close" onClick={() => setOpen(false)}></CloseIcon>
+                    </div>
+                    <div>
+                        <Button onClick={handleDeleteConfirmClick}>Confirm</Button>
+                    </div>
+                </Popup>
+            </div>
         </div>
     )
 }
