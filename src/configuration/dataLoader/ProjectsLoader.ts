3a563990452b9a857ade33ecf66197305ea79d2d
markopolo139 <marekseget@onet.pl>
2023-10-01 22:03:01 +0200

    feat(dataLoader): create project loader

---
+++ src/configuration/dataLoader/ProjectsLoader.ts
@@ -0,0 +1,17 @@
+import {projectApi} from "../../api/ProjectApi";
+import {store} from "../Strore";
+import {setNewProjects} from "../../slices/ProjectsSlice";
+
+export async function projectsLoader() {
+    const promise = store.dispatch(projectApi.endpoints?.getProjects.initiate())
+    try {
+        const result = await promise.unwrap()
+        store.dispatch(setNewProjects(result))
+        return result
+    } catch (e) {
+        console.log("Error in project loader")
+        return []
+    } finally {
+        promise.unsubscribe()
+    }
+}
